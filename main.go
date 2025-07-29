9a7b5bbd119d34774b9bce101292860eed5543da
Dmitriy Chekanov <dmit.checkanov@yandex.ru>
2025-07-29 13:51:42 +0300

    Проектирование и создание БД

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/Dmitry-CH/go-final-project/pkg/db"
 	"github.com/joho/godotenv"
 )
 
@@ -17,8 +18,15 @@ var port = 7540
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Print("Error loading .env file")
 	}
+
+	err = db.Init()
+	if err != nil {
+		log.Fatalf("Start db error: %s", err.Error())
+		return
+	}
+	defer db.Close()
 
 	envPort := os.Getenv("TODO_PORT")
 	if len(envPort) > 0 {
@@ -32,6 +40,5 @@ func main() {
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatalf("Start server error: %s", err.Error())
-		return
 	}
 }
---
+++ pkg/db/db.go
@@ -0,0 +1,53 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+
+	_ "modernc.org/sqlite"
+)
+
+const schema = `CREATE TABLE scheduler (
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					date CHAR(8) NOT NULL DEFAULT "",
+					title VARCHAR(256) NOT NULL DEFAULT "",
+					comment TEXT NOT NULL DEFAULT "",
+					repeat VARCHAR(128) NOT NULL DEFAULT ""
+);
+
+CREATE INDEX idx_scheduler_date ON scheduler (date);`
+
+var db *sql.DB
+var dbFile = "scheduler.db"
+
+func Init() error {
+	_, err := os.Stat(dbFile)
+
+	var install bool
+	if err != nil {
+		install = true
+	}
+
+	envDbFile := os.Getenv("TODO_DBFILE")
+	if len(envDbFile) > 0 {
+		dbFile = envDbFile
+	}
+
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		return err
+	}
+
+	if install {
+		_, err = db.Exec(schema)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func Close() error {
+	return db.Close()
+}
